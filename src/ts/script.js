5f11e85ed4d29346a95e7926219f6c089dbf89b6
YoussefEchelhi <114957292+YoussefEchelhi@users.noreply.github.com>
2023-02-26 11:25:55 +0100

    Added function

---
+++ src/ts/script.js
@@ -0,0 +1,23 @@
+document.querySelector('.btn1').addEventListener('click', showAvatar);
+document.querySelector('.btn2').addEventListener('click', showOtto);
+document.querySelector('.btn3').addEventListener('click', showTopGun);
+document.querySelector('.btn4').addEventListener('click', showPlane);
+
+
+function showAvatar(){
+    window.open('https://www.youtube.com/watch?v=d9MyW72ELq0', '_blank');
+}
+
+function showOtto(){
+    window.open('https://www.youtube.com/watch?v=eFYUX9l-m5I', '_blank');
+}
+
+function showTopGun(){
+    window.open('https://www.youtube.com/watch?v=giXco2jaZ_4', '_blank');
+}
+
+function showPlane(){
+    window.open('https://www.youtube.com/watch?v=M25zXBIUVr0', '_blank');
+}
+
+
